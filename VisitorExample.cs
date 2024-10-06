12532c9e1254a9102f4bcbb33ba38c929f58f81f
OksanaTytaruk <cassieroyster@gmail.com>
2024-10-07 01:30:48 +0300

    Create VisitorExample.cs

---
+++ VisitorExample.cs
@@ -0,0 +1,70 @@
+using System;
+
+// Інтерфейс відвідувача
+public interface IVisitor
+{
+    void Visit(ElementA element);
+    void Visit(ElementB element);
+}
+
+// Абстрактний клас елемента
+public abstract class Element
+{
+    public abstract void Accept(IVisitor visitor);
+}
+
+// Конкретний клас елемента A
+public class ElementA : Element
+{
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+
+    public void OperationA()
+    {
+        Console.WriteLine("Виконання операції A");
+    }
+}
+
+// Конкретний клас елемента B
+public class ElementB : Element
+{
+    public override void Accept(IVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+
+    public void OperationB()
+    {
+        Console.WriteLine("Виконання операції B");
+    }
+}
+
+// Конкретний клас відвідувача
+public class ConcreteVisitor : IVisitor
+{
+    public void Visit(ElementA element)
+    {
+        element.OperationA();
+    }
+
+    public void Visit(ElementB element)
+    {
+        element.OperationB();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Element[] elements = new Element[] { new ElementA(), new ElementB() };
+        ConcreteVisitor visitor = new ConcreteVisitor();
+
+        foreach (var element in elements)
+        {
+            element.Accept(visitor);
+        }
+    }
+}
